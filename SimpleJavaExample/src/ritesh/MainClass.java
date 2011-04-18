fd20b4f4e47f0a03dbb7a801ced0b5559bec5ead
ritesh.uml <ritesh.uml@gmail.com>
2011-04-18 18:15:49 +0000

    hi, commiting two programs

---
+++ SimpleJavaExample/src/ritesh/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ritesh;
+
+/**
+ * @author RITESH
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
