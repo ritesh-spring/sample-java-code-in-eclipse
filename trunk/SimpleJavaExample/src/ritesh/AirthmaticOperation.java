194fd9078b0ea87d8982182796fb654dd2202141
ritesh.uml@gmail.com <ritesh.uml@gmail.com@0ba70c7f-35e4-7fcc-6d09-3cea83f81c21>
2011-04-18 18:15:49 +0000

    hi, commiting two programs

---
+++ trunk/SimpleJavaExample/src/ritesh/AirthmaticOperation.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ritesh;
+
+/**
+ * @author RITESH
+ *
+ */
+public class AirthmaticOperation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hiiii");
+	}
+
+}
