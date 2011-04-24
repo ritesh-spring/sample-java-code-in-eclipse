7ca9c25e56f1791b406f845fa8e91ef2131c7901
ritesh.uml@gmail.com <ritesh.uml@gmail.com@0ba70c7f-35e4-7fcc-6d09-3cea83f81c21>
2011-04-24 20:04:22 +0000

    hhh

---
+++ trunk/SimpleJavaExample/src/ritesh/ReverseAString.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package ritesh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author RITESH
+ *
+ */
+public class ReverseAString {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter your name:");
+		String name=br.readLine();
+		System.out.println("My name is:"+name);
+	}
+
+}
