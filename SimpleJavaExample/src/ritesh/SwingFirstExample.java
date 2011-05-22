a888f86dabe09c6e26cbbd39b6829be2f3fef56a
ritesh.uml <ritesh.uml@gmail.com>
2011-05-22 00:52:14 +0000

    Now Uploading Image Frame which shows the Image in Frame

---
+++ SimpleJavaExample/src/ritesh/SwingFirstExample.java
@@ -0,0 +1,18 @@
+package ritesh;
+
+import javax.swing.JFrame;
+
+public class SwingFirstExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+JFrame obj=new JFrame("Ritesh Swing Frame");
+obj.setSize(400, 300);
+obj.setVisible(true);
+
+	}
+
+}
---
+++ SimpleJavaExample/src/ritesh/SwingSecondExample.java
@@ -0,0 +1,18 @@
+package ritesh;
+
+import javax.swing.JFrame;
+
+public class SwingSecondExample extends JFrame{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SwingSecondExample sse=new SwingSecondExample();
+ sse.setTitle("Ritesh Second Swing Frame");
+ sse.setSize(400, 300);
+ sse.setVisible(true);
+	}
+
+}
