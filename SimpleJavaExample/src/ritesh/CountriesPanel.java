9f21eb19acd61b90e6b1f618986bd6237dac9541
ritesh.uml <ritesh.uml@gmail.com>
2011-05-24 09:38:11 +0000

    Tabbed Pane Example

---
+++ SimpleJavaExample/src/ritesh/CountriesPanel.java
@@ -0,0 +1,16 @@
+package ritesh;
+import javax.swing.*;
+
+class CountriesPanel extends JPanel{
+
+	public CountriesPanel() {
+		// TODO Auto-generated constructor stub
+		JButton b1=new JButton("India");
+		JButton b2=new JButton("USA");
+		JButton b3=new JButton("Japan");
+		add(b1);
+		add(b2);
+		add(b3);
+	}
+
+}
