70a324469f513d6b55de779d9a91154588f864b4
ritesh.uml <ritesh.uml@gmail.com>
2011-07-02 12:36:49 +0000

    This Commit includes Some Thread Examples

--- CoreJavaExamples/src/ritesh/Starter.java
+++ CoreJavaExamples/src/ritesh/Starter.java
@@ -4,7 +4,7 @@ public class Starter extends Thread {
 	 private int x= 2;
 	 public static void main(String[] args) throws Exception {
 	 new Starter().makeItSo();
-	 System.out.println("My name is Mr. X");
+	 System.out.println("My name is MR. Main Thread ");
 	 }
 	 public Starter() {
 	 x=5;
@@ -16,5 +16,6 @@ public class Starter extends Thread {
 	 System.out.println("My name is Mr. Y");
 	 System.out.println(x);
 	 }
-	 public void run() { x *= 2; }
+	 public void run() { x *= 2;
+	 System.out.println("Value of x in child Thread..........."+x);}
 }
