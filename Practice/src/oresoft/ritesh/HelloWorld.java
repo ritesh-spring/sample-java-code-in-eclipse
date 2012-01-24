1bb549fe2f8488a7cb57c9e9fa3992f14af625fd
ritesh.uml@gmail.com <ritesh.uml@gmail.com@0ba70c7f-35e4-7fcc-6d09-3cea83f81c21>
2012-01-24 07:33:23 +0000

    First Export

---
+++ Practice/src/oresoft/ritesh/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package oresoft.ritesh;
+
+/**
+ * @author Ritesh Kumar
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi I am Ritesh");
+	}
+
+}
